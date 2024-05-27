b6c4d044b3d2bc71d826d205d380e332f42fe5e8
AreaSamsara <AreaSamsara@outlook.com>
2024-05-27 19:23:21 +0800

    跟着sylar完成了bytearray系统的开发，测试通过

--- main.cpp
+++ main.cpp
@@ -1,30 +1,170 @@
-//#include "address.h"
-//#include "log.h"
-//#include "singleton.h"
-//#include "socket.h"
-//#include "iomanager.h"
-//
-//using namespace AddressSpace;
-//using namespace LogSpace;
-//using namespace SingletonSpace;
-//using namespace SocketSpace;
-//using namespace IOManagerSpace;
-//
-//
-//template<class T>
-////生成bits位掩码
-//static T CreateMask(uint32_t bits)
-//{
-//	return (1 << (sizeof(T) * 8 - bits)) - 1;
-//}
-//
-//int main(int argc, char** argv)
-//{
-//	//将当前地址与掩码取与，得到网段地址
-//	shared_ptr<IPv4Address> address;
-//	uint32_t prefix_len = 1;
-//	auto x = CreateMask<uint32_t>(prefix_len);
-//
-//	cout << x;
-//	return 0;
-//}
+#include "bytearray.h"
+#include "utility.h"
+#include "log.h"
+#include "utility.h"
+#include "singleton.h"
+
+using namespace ByteArraySpace; 
+using namespace UtilitySpace;
+using namespace SingletonSpace;
+using namespace LogSpace;
+using std::shared_ptr;
+
+
+void test()
+{
+	int count = 100, base_size = 1;
+
+
+	vector<int64_t> vec;
+	//获取多个随机数
+	for (int i = 0; i < count; ++i)
+	{
+		vec.push_back(rand());
+	}
+
+	shared_ptr<ByteArray> bytearray(new ByteArray(base_size));
+	//将所有随机数依次写入
+	for (auto& i : vec)
+	{
+		bytearray->writeInt64(i);
+	}
+
+	bytearray->setPosition(0);
+
+
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		//读取随机数并打印
+		int32_t value = bytearray->readInt64();
+
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		log_event->getSstream() << i << "-" << value << "-" << (int32_t)vec[i];
+		Singleton<LoggerManager>::GetInstance_normal_ptr()->getDefault_logger()->log(LogLevel::INFO, log_event);
+
+		//value应该等于vec[i]，否则报错
+		if (value != vec[i])
+		{
+			shared_ptr<LogEvent> event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+			Assert(event);
+		}
+	}
+
+	//此时可读数据的大小应该为零（数据被读完），否则报错
+	if (bytearray->getRead_size() != 0)
+	{
+		shared_ptr<LogEvent> event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(event);
+	}
+
+
+	shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+	log_event->getSstream() << "write/read count=" << count << " base_size=" << base_size << " size=" << bytearray->getSize();
+	Singleton<LoggerManager>::GetInstance_normal_ptr()->getDefault_logger()->log(LogLevel::INFO, log_event);
+
+
+	
+}
+
+void test_file()
+{
+	int count = 100, base_size = 1;
+
+
+	vector<int32_t> vec;
+	//获取多个随机数
+	for (int i = 0; i < count; ++i)
+	{
+		vec.push_back(rand());
+	}
+	shared_ptr<ByteArray> bytearray(new ByteArray(base_size));
+	//将所有随机数依次写入
+	for (auto& i : vec)
+	{
+		bytearray->writeFint32(i);
+	}
+
+	bytearray->setPosition(0);
+
+
+	for (size_t i = 0; i < vec.size(); ++i)
+	{
+		//读取随机数
+		int32_t value = bytearray->readFint32();
+
+		//value应该等于vec[i]，否则报错
+		if (value != vec[i])
+		{
+			shared_ptr<LogEvent> event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+			Assert(event);
+		}
+	}
+
+	//此时可读数据的大小应该为零（数据被读完），否则报错
+	if (bytearray->getRead_size() != 0)
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+
+	shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+	log_event->getSstream() << "write/read count=" << count << " base_size=" << base_size << " size=" << bytearray->getSize();
+	Singleton<LoggerManager>::GetInstance_normal_ptr()->getDefault_logger()->log(LogLevel::INFO, log_event);
+
+
+
+
+	bytearray->setPosition(0);
+
+	if (!bytearray->writeToFile("./test_bytearray.txt"))
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+
+	shared_ptr<ByteArray> bytearray2(new ByteArray(base_size * 2));
+
+	if (!bytearray2->readFromFile("./test_bytearray.txt"))
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+
+	bytearray2->setPosition(0);
+
+
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		log_event->getSstream() << "bytearray:" << bytearray->toString();
+		Singleton<LoggerManager>::GetInstance_normal_ptr()->getDefault_logger()->log(LogLevel::INFO, log_event);
+		log_event->setSstream("");
+		log_event->getSstream() << "bytearray2:" << bytearray2->toString();
+		Singleton<LoggerManager>::GetInstance_normal_ptr()->getDefault_logger()->log(LogLevel::INFO, log_event);
+	}
+
+	if (bytearray->toString() != bytearray2->toString())
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+
+	if (bytearray->getPosition() != 0)
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+
+	if (bytearray2->getPosition() != 0)
+	{
+		shared_ptr<LogEvent> log_event(new LogEvent(__FILE__, __LINE__, GetThread_id(), GetThread_name(), GetFiber_id(), 0, time(0)));
+		Assert(log_event);
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	//test();
+	test_file();
+	return 0;
+}
