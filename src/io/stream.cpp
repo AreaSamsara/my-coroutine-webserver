6780e44b16d2c040ed0d5581ee80669d8ece6b38
AreaSamsara <AreaSamsara@outlook.com>
2025-03-02 22:16:25 +0800

    恢复stream模块的注释

--- src/io/stream.cpp
+++ src/io/stream.cpp
@@ -9,7 +9,7 @@ namespace StreamSpace
 		while (length_to_read > 0)
 		{
 			size_t return_value = read((char *)buffer + offset, length_to_read);
-			// ���read()����������ֱ�ӷ��ظ�ֵ
+			// 如果read()函数出错，直接返回该值
 			if (return_value <= 0)
 			{
 				return return_value;
@@ -25,7 +25,7 @@ namespace StreamSpace
 		while (length_to_read > 0)
 		{
 			size_t return_value = read(bytearray, length_to_read);
-			// ���read()����������ֱ�ӷ��ظ�ֵ
+			// 如果read()函数出错，直接返回该值
 			if (return_value <= 0)
 			{
 				return return_value;
@@ -42,7 +42,7 @@ namespace StreamSpace
 		while (length_to_write > 0)
 		{
 			size_t return_value = write((char *)buffer + offset, length_to_write);
-			// ���write()����������ֱ�ӷ��ظ�ֵ
+			// 如果write()函数出错，直接返回该值
 			if (return_value <= 0)
 			{
 				return return_value;
@@ -58,7 +58,7 @@ namespace StreamSpace
 		while (length_to_write > 0)
 		{
 			size_t return_value = write(bytearray, length_to_write);
-			// ���write()����������ֱ�ӷ��ظ�ֵ
+			// 如果write()函数出错，直接返回该值
 			if (return_value <= 0)
 			{
 				return return_value;
